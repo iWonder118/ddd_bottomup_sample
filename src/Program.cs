2cd247e2531bbe612bffbf7af00a20ffbaa0a273
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2023-12-31 18:22:09 +0900

    add override Equals methods

--- src/Program.cs
+++ src/Program.cs
@@ -6,14 +6,20 @@
 
         static void Main()
         {
-            var fullName = new FullName("smith", "john");
-            Console.WriteLine(fullName.LastName);
+            FullName nameA = new FullName("i", "wonder");
+            FullName nameB = new FullName("john", "smith");
+            
+            bool compareResult = nameA.Equals(nameB);
+            Console.WriteLine(compareResult);
 
+            bool compareResult2 = nameA  == nameB;
+            Console.WriteLine(compareResult2);
+            
         }
     }
 }
 
-class FullName
+class FullName : IEquatable<FullName>
 {
     public FullName(string firstName, string lastName)
     {
@@ -23,5 +29,30 @@ class FullName
 
     public string FirstName { get; }
     public string LastName { get; }
+
+    public bool Equals(FullName other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return string.Equals(FirstName, other.FirstName)
+            && string.Equals(LastName, other.LastName);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((FullName) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return ((FirstName != null ? FirstName.GetHashCode() : 0) * 397) 
+                ^ (LastName != null ? LastName.GetHashCode() : 0);
+        }
+    }
 }
 
