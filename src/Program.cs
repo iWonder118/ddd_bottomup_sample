47e42b58daa7d50437613489aeb7f36023252be7
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2023-12-31 11:44:34 +0900

    create FullName class

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,27 @@
-﻿String filename = "john smith";
-String[] tokens = filename.Split(" ");
-String lastname = tokens[0];
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Main();
 
-Console.WriteLine(lastname);
+        static void Main()
+        {
+            var fullName = new FullName("smith", "john");
+            Console.WriteLine(fullName.LastName);
+
+        }
+    }
+}
+
+class FullName
+{
+    public FullName(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public string FirstName { get; }
+    public string LastName { get; }
+}
+
